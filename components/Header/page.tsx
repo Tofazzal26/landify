5f76e5a324abece1271c2df32d737ec74d483757
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-10-02 17:03:52 +0600

    landify header style finish

---
+++ components/Header/page.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <div className="bg-base-100 shadow-sm">
+        <div className="navbar container mx-auto py-4">
+          <div className="navbar-start">
+            <div className="flex items-center gap-10">
+              <div className="flex items-center gap-2">
+                <Image src={"/land.png"} width={30} height={30} alt="logo" />
+                <h2 className="text-2xl font-semibold">Landify</h2>
+              </div>
+              <div className="lg:flex hidden">
+                <ul className="font-semibold text-[#555758] flex items-center gap-8">
+                  <li>Company</li>
+                  <li>Solutions</li>
+                  <li>Pricing</li>
+                  <li>Resources</li>
+                  <li>Contact</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-center hidden lg:flex">
+              <div className="space-x-2">
+                <button className="bg-[#e9bb71] text-base font-semibold px-4 py-3 rounded-md">
+                  Free consultation
+                </button>
+                <button className="bg-[#295c51] text-white text-base font-semibold px-4 py-3 rounded-md">
+                  {"Let's"} connect
+                </button>
+              </div>
+            </div>
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />{" "}
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow translate-x-[-150px] space-y-2 text-[#555758] font-semibold cursor-pointer"
+              >
+                <li>Company</li>
+                <li>Solutions</li>
+                <li>Pricing</li>
+                <li>Resources</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

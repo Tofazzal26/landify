fd21ecd0604938281f0bed0d44cf5bd19219218a
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-10-03 22:47:18 +0600

    all device responsibe finish banner

--- components/Banner/page.tsx
+++ components/Banner/page.tsx
@@ -3,24 +3,24 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="container mx-auto">
-      <div className="flex items-center justify-between mt-16">
+    <div className="container mx-auto lg:px-0 md:px-2 px-2">
+      <div className="flex lg:flex-row md:flex-row flex-col items-center justify-between lg:mt-16 md:mt-8 mt-4">
         <div>
           <div>
-            <h4 className="text-lg font-medium text-[#295c51]">
+            <h4 className="lg:text-lg md:text-base text-base font-medium text-[#295c51]">
               Start earning more traffic
             </h4>
-            <h2 className="text-[60px] font-semibold w-[650px] leading-[70px] mt-4">
+            <h2 className="lg:text-[60px] md:text-[30px] text-lg font-semibold lg:w-[650px] md:w-[400px] w-[200px] lg:leading-[70px] md:leading-[45px] lg:mt-4 md:mt-3 mt-2">
               Marketing revolution. Unlimited success ✌️
             </h2>
-            <p className="text-lg font-normal text-[#5b5c5e] w-[450px] mt-4 mb-8">
+            <p className="text-lg font-normal text-[#5b5c5e] lg:w-[450px] md:w-[300px] lg:mt-4 lg:mb-8 mb-6 mt-2">
               Join us in revolutionizing your marketing strategies for unlimited
               success. {"Let's"} achieve your goals together.
             </p>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex lg:flex-row md:flex-col flex-col lg:items-center gap-4">
             <div>
-              <button className="bg-[#e9bb71] text-base text-[#554b3a] font-semibold px-5 py-4 rounded-md">
+              <button className="bg-[#e9bb71] text-base text-[#554b3a] font-semibold lg:px-5 lg:py-4 px-3 py-3 rounded-md">
                 Free consultation
               </button>
             </div>
@@ -64,7 +64,35 @@ const Banner = () => {
             width={500}
             height={500}
             alt="landpress"
+            className="lg:w-[500px] lg:h-[500px] md:h-[400px] h-[300px]"
           />
+        </div>
+      </div>
+      <div className="lg:mt-20 mt-10">
+        <div className="flex lg:flex-row md:flex-wrap flex-wrap items-center justify-between">
+          <div className="lg:border-r-2 border-[#c9d6d3] lg:w-[250px]">
+            <Image src={"/affrin.svg"} width={100} height={100} alt="brand" />
+          </div>
+          <div className="lg:border-r-2 border-[#c9d6d3] lg:w-[250px]">
+            <Image
+              src={"/microsoft.svg"}
+              width={100}
+              height={100}
+              alt="brand"
+            />
+          </div>
+          <div className="lg:border-r-2 border-[#c9d6d3] lg:w-[250px]">
+            <Image src={"/novo.svg"} width={100} height={100} alt="brand" />
+          </div>
+          <div className="lg:border-r-2 border-[#c9d6d3] lg:w-[250px]">
+            <Image src={"/shofify.svg"} width={100} height={100} alt="brand" />
+          </div>
+          <div>
+            <Image src={"/coinbase.svg"} width={100} height={100} alt="brand" />
+          </div>
+          <div>
+            <Image src={"/unit.svg"} width={100} height={100} alt="brand" />
+          </div>
         </div>
       </div>
     </div>
